a95e21d6bdac18f37b65d404cc3fa25a4bf9859a
Arnav Maurya <22AMaurya@heckgrammar.co.uk>
2025-04-04 10:06:31 +0100

    time

--- BlankCSDotNet8/Program.cs
+++ BlankCSDotNet8/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            
+            DateTime current = DateTime.Now;
+            Console.WriteLine("What is the target year in 4 digits");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the target month (in number)");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the target day (Number date)");
+            int day = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the target hour (Number)");
+            int hour = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the target minutes (Number)");
+            int minutes = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the target seconds (Number)");
+
+            int seconds = Convert.ToInt32(Console.ReadLine());
+            DateTime target = new DateTime(year, month, day, hour, minutes, seconds);
+
+            for (int i = 1; i > 0; i++)
+            {
+                current = DateTime.Now;
+                Console.WriteLine((target - current));
+                Thread.Sleep(1000);
+                Console.Clear();
+
+            }
         }
     }
 }
